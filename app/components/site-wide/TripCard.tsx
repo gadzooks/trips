eea09e6d1f39e4b814ca6e983a07d130ff9cf586
Amit Karwande <akarwande@coredigital.com>
2025-01-20 00:54:24 -0800

    same

--- app/components/site-wide/TripCard.tsx
+++ app/components/site-wide/TripCard.tsx
@@ -7,7 +7,7 @@ interface TripCardProps {
   export function TripCard({ title, date }: TripCardProps) {
     return (
       <div className="p-4 bg-gray-50 dark:bg-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
-        <h3 className="font-medium">{title}</h3>
+        <h3 className="font-medium text-transparent bg-clip-text bg-blue-400">{title}</h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">{date}</p>
       </div>
     )
