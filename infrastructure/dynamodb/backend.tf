abe570520053983eacfa6ec0196daa1aabb8c453
Amit Karwande <akarwande@coredigital.com>
2025-02-09 01:17:03 -0800

    fix terraform -out

--- infrastructure/dynamodb/backend.tf
+++ infrastructure/dynamodb/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "trip-planner-terraform-state"
-    key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "trip-planner-terraform-locks"
     encrypt        = true
